d4db2f69c24f4a78c4ab888ddc07d14e8b0b9d9e
Matti Benoliel <benolielmatti@hotmail.fr>
2020-03-16 16:45:20 +0100

    controller devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  skip_before_action :authenticate_user!, only: :home
+  before_action :authenticate_user!, except: :home
 
    def home
    end
