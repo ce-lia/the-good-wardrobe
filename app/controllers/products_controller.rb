321d5ea3c82ddd7ec358610737d14348b4867ea5
Patrick Blanc <perso@patrickblanc.net>
2020-03-18 14:49:01 +0100

    crud done

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,35 +1,50 @@
 class ProductsController < ApplicationController
 
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+
   def index #=> view => index.html.erb
     @products = Product.all
   end
 
-  def show
-    @product = Product.find(params[:id])
+  def show; end
+
+  def new
+    @product = Product.new
   end
 
   def create
     @product = Product.new(product_params)
-    @product.save
-    redirect_to products_path
+    @product.user = current_user
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
   end
 
   def edit; end
 
   def update
-    @product.update(product_params)
-    redirect_to product_path(@product)
+    if @product.update(product_params)
+      redirect_to product_path(@product)
+    else
+      render :edit
+    end
   end
+
   def destroy
     @product.destroy
     redirect_to products_path
   end
+
+
   private
+
   def set_product
     @product = Product.find(params[:id])
   end
   def product_params
-    params.require(:product).permit(:name, :address, :description, :chef)
+    params.require(:product).permit(:title, :description, :category, :second_hand, :status)
   end
 
 
