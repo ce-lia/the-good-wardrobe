b9a42d4dd72dfc70627a9d63973dc831b8b9703f
Matti Benoliel <benolielmatti@hotmail.fr>
2020-03-20 11:30:26 +0100

    homepage

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   def index
-    @products = Product.all
+    @products = user.products
   end
 
   def show; end
