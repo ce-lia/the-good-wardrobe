174e817c6118fb61f409d6d8070ed1385d19dce3
vanmak <vanmakdavid.lwg@gmail.com>
2020-03-24 17:23:30 +0100

    btn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,70 +2,39 @@ import "bootstrap";
 import $ from "jquery";
 
 
+// /* Code pour les boutons de materialize */
 
-/* Fucking jQuery */
-
-const header = document.querySelector('.start-style')
-if (header) {
-
-
-  window.addEventListener("scroll", (event) => {
-    if (window.scrollY >=10) {
-      header.classList.remove("start-style");
-      header.classList.add("scroll-on");
-      console.log("Test if")
-    }
-    else {
-      header.classList.add('start-style');
-      header.classList.remove('scroll-on');
-      console.log("Test else")
-    }
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, options);
   });
-}
 
-// (function($) { "use strict";
 
-//   $(function() {
-//     var header = $(".start-style");
-//     $(window).scroll(function() {
-//       var scroll = $(window).scrollTop();
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      direction: 'left'
+    });
+  });
 
-//       if (scroll >= 10) {
-//         header.removeClass('start-style').addClass("scroll-on");
-//       } else {
-//         header.removeClass("scroll-on").addClass('start-style');
-//       }
-//     });
-//   });
 
-//   //Animation
+// /* Code pour le stretch de la navbar 2 */
 
-//   $(document).ready(function() {
-//     $('body.hero-anime').removeClass('hero-anime');
-//   });
+// const header = document.querySelector('.start-style')
+// if (header) {
 
-//   //Menu On Hover
 
-//   $('body').on('mouseenter mouseleave','.nav-item',function(e){
-//       if ($(window).width() > 750) {
-//         var _d=$(e.target).closest('.nav-item');_d.addClass('show');
-//         setTimeout(function(){
-//         _d[_d.is(':hover')?'addClass':'removeClass']('show');
-//         },1);
-//       }
-//   });
-
-//   //Switch light/dark
-
-//   $("#switch").on('click', function () {
-//     if ($("body").hasClass("dark")) {
-//       $("body").removeClass("dark");
-//       $("#switch").removeClass("switched");
+//   window.addEventListener("scroll", (event) => {
+//     if (window.scrollY >=10) {
+//       header.classList.remove("start-style");
+//       header.classList.add("scroll-on");
+//       console.log("Test if")
 //     }
 //     else {
-//       $("body").addClass("dark");
-//       $("#switch").addClass("switched");
+//       header.classList.add('start-style');
+//       header.classList.remove('scroll-on');
+//       console.log("Test else")
 //     }
 //   });
-
-//   })(jQuery);
+// }
+// /* Code pour le stretch de la navbar 2 */
